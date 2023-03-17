9cc1005d892ca633805351aa56b741d839cb2c97
Jordan Pierce <jordan.pierce@noaa.gov>
2023-03-17 14:00:07 -0400

    Metashape workflow with georeferencing

--- georeference_images.py
+++ georeference_images.py
@@ -1,4 +1,5 @@
 from classes.monoplotting import TowLine
+from scripts.sfm_workflow import *
 import argparse
 
 
@@ -11,37 +12,44 @@ if __name__ == "__main__":
     parser.add_argument("--out_dir", "-o",
         type=str,
         help="Path to a directory where output files will be saved.")
+
     parser.add_argument("--usbl_path", "-u",
         type=str,
         help="Path to a GIS file containing USBL GPS points.")
+
     parser.add_argument("--datetime_field", "-df",
         type=str,
-        default="DateTime",
+        default="GUDateTime",
         help="Name of the field in the USBL attribute table that contains temporal \
                 information. Default value is 'DateTime'.")
+
     parser.add_argument("--pdop_field", "-pf",
         type=str,
         default=None,
         help="Name of the field in the USBL attribute table that contains some form \
             of positional accuracy, most often in the form of a PDOP value. Default \
             value is 'Max_PDOP'.")
+
     parser.add_argument("--elevation_field", "-ef",
                         type=str,
-                        default="CaCorAlt",
+                        default="CaAltCor_m",
                         help="Name of the field in the USBL attribute table that \
                         contains elevation (depth) values. Default value is \
                         'CaCorAlt'.")
+
     parser.add_argument("--filter_quartile", "-f",
         type=float,
         default=0.95,
         help="A value between 0.0 and 1.0 that controls the filtering of USBL points \
                 based on a user-specified --pdop_field (-pf). If set to 1.0 then no \
                 filtering will be applied. Default value is 0.95 (keep 95% of values).")
+
     parser.add_argument("--process_noise_std", "-p",
         type=float,
         default=1.0,
         help="One of two parameters that controls the smoothing of tracklines. If set \
                 to 0.0, no smoothing will be applied. Default value is 1.0.")
+
     parser.add_argument("--measurement_noise_std", "-m",
         type=float,
         default=0.25,
@@ -57,10 +65,15 @@ if __name__ == "__main__":
     # the input arguments. This processing chain computes vector files (GDFs) and meta-
     # data (stored in GDF attribute tables) that pertain to georeferencing of imagery.
 
-    towline = TowLine(args.image_dir, args.out_dir, args.usbl_path,
-                    args.datetime_field, args.pdop_field, args.elevation_field,
-                    args.filter_quartile, args.process_noise_std,
-                    args.measurement_noise_std)
+    towline = TowLine(args.image_dir,
+                      args.out_dir,
+                      args.usbl_path,
+                      args.datetime_field,
+                      args.pdop_field,
+                      args.elevation_field,
+                      args.filter_quartile,
+                      args.process_noise_std,
+                      args.measurement_noise_std)
 
     # Currently a TowLine object has the following exposed methods:
     #  - write_georeferenced_images() - to write georeferenced images to disk
@@ -85,3 +98,6 @@ if __name__ == "__main__":
 
     # Dump the GDFs...
     towline.dump_gdfs()
+
+    # Run the SfMm Workflow
+    run_sfm_workflow(args.image_dir)
--- scripts/sfm_workflow.py
+++ scripts/sfm_workflow.py
@@ -1,6 +1,10 @@
 import os, sys, time
 import Metashape
 
+from create_tif import *
+from create_reference import *
+
+# Star the timer
 t0 = time.time()
 
 # Get the Metashape License stored in the environmental variable
@@ -20,66 +24,88 @@ def find_files(folder, types):
     return [entry.path for entry in os.scandir(folder) if
             (entry.is_file() and os.path.splitext(entry.name)[1].lower() in types)]
 
-# Check if the number of command-line arguments is less than 3.
-# If true, print usage instructions and exit the program with a status code of 1.
-if len(sys.argv) < 2:
-    print("Usage: general_workflow.py <image_folder>")
-    sys.exit(1)
 
-# Get the image folder and output folder paths from command-line arguments.
-image_folder = output_folder = sys.argv[1]
+def run_sfm_workflow(input_folder):
+    """Takes in an input folder, runs SfM Workflow on all images in it,
+    outputs the results in the same folder."""
 
-# Call the "find_files" function to get a list of photo file paths
-# with specified extensions from the image folder.
-photos = find_files(image_folder, [".jpg", ".jpeg", ".tif", ".tiff"])
+    # Get the image folder and output folder paths from command-line arguments.
+    image_folder = output_folder = input_folder
 
-# Create a new Metashape document and save it as a project file in the output folder.
-doc = Metashape.Document()
-doc.save(output_folder + '/project.psx')
+    # Create the reference csv, get the path.
+    reference_path = create_reference_csv(image_folder)
 
-# Create a new chunk (3D model) in the Metashape document.
-if doc.chunk is None:
-    chunk = doc.addChunk()
+    # Call the "find_files" function to get a list of photo file paths
+    # with specified extensions from the image folder. ".jpeg", ".tif", ".tiff"
+    photos = find_files(image_folder, [".jpg"])
 
-# Add the photos to the chunk.
-if not chunk.cameras:
-    chunk.addPhotos(photos)
+    # Create a new Metashape document and save it as a project file in the output folder.
+    doc = Metashape.Document()
+    doc.save(output_folder + '/project.psx')
 
-# Save the Metashape document.
-doc.save()
+    # Create a new chunk (3D model) in the Metashape document.
+    if doc.chunk is None:
+        chunk = doc.addChunk()
+        chunk.crs = Metashape.CoordinateSystem("EPSG::32655")
+        doc.save()
 
-# Print the number of images that were successfully loaded into the chunk.
-print(str(len(chunk.cameras)) + " images loaded")
+    # Add the photos to the chunk.
+    if not chunk.cameras:
+        chunk.addPhotos(photos)
+        doc.save()
 
-# Match the photos by finding common features and establishing correspondences.
-if not chunk.tie_points:
-    chunk.matchPhotos(keypoint_limit=40000, tiepoint_limit=10000, generic_preselection=True, reference_preselection=True)
-    doc.save()
+    # Print the number of images that were successfully loaded into the chunk.
+    print(str(len(chunk.cameras)) + " images loaded")
 
-    # Align the cameras to estimate their relative positions in space.
-    chunk.alignCameras()
-    doc.save()
+    if chunk.cameras:
+        chunk.importReference(reference_path,
+                              format=Metashape.ReferenceFormatCSV,
+                              columns='nxyz[XYZ]',
+                              delimiter=',')
+        doc.save()
 
-# Build depth maps (2.5D representations of the scene) from the aligned photos.
-if chunk.tie_points and not chunk.depth_maps:
-    chunk.buildDepthMaps(downscale=2, filter_mode=Metashape.MildFiltering)
-    doc.save()
+    # Match the photos by finding common features and establishing correspondences.
+    if not chunk.tie_points:
+        chunk.matchPhotos(keypoint_limit=40000,
+                          tiepoint_limit=10000,
+                          generic_preselection=True,
+                          reference_preselection=True)
+        doc.save()
 
-# Build a 3D model from the depth maps.
-if chunk.depth_maps and not chunk.model:
-    chunk.buildModel(source_data=Metashape.DepthMapsData)
-    doc.save()
+        # Align the cameras to estimate their relative positions in space.
+        chunk.alignCameras()
+        doc.save()
 
-# Build a orthomosaic from the 3D model.
-if chunk.model and not chunk.orthomosaic:
-    chunk.buildOrthomosaic(surface_data=Metashape.ModelData)
-    doc.save()
+    # Build depth maps (2.5D representations of the scene) from the aligned photos.
+    if chunk.tie_points and not chunk.depth_maps:
+        chunk.buildDepthMaps(downscale=1, filter_mode=Metashape.MildFiltering)
+        doc.save()
 
-# Export the orthomosaic as a GeoTIFF file if it exists in the chunk.
-if chunk.orthomosaic:
-    chunk.exportRaster(output_folder + '/orthomosaic.tif', source_data=Metashape.OrthomosaicData)
+    # Build a 3D model from the depth maps.
+    if chunk.depth_maps and not chunk.model:
+        chunk.buildModel(source_data=Metashape.DepthMapsData)
+        doc.save()
 
-# Print a message indicating that the processing has finished and the results have been saved.
-print('Processing finished, results saved to ' + output_folder + '.')
-print("Time: ", (time.time() - t0) / 60)
+    # Build a orthomosaic from the 3D model.
+    if chunk.model and not chunk.orthomosaic:
+        chunk.buildOrthomosaic(surface_data=Metashape.ModelData)
+        doc.save()
 
+    # Export the orthomosaic as a GeoTIFF file if it exists in the chunk.
+    if chunk.orthomosaic:
+        chunk.exportRaster(output_folder + '/Orthomosaic.tif', source_data=Metashape.OrthomosaicData)
+
+    # Print a message indicating that the processing has finished and the results have been saved.
+    print('Processing finished, results saved to ' + output_folder + '.')
+    print("Time: ", (time.time() - t0) / 60)
+
+
+if __name__ == '__main__':
+
+    # Check if the number of command-line arguments is less than 3.
+    # If true, print usage instructions and exit the program with a status code of 1.
+    if len(sys.argv) != 2:
+        print("Usage: sfm_workflow.py <image_folder>")
+        sys.exit(1)
+
+    run_sfm_workflow(sys.argv[1])
